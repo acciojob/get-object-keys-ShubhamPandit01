32d007d1d8b9018b72fb883897de49b8639dd778
Shubham Pandit <Shubhampandit75572@gmail.com>
2025-03-22 12:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ const student = {
 };
 
 // Function to get all keys from the object
-const myObj = {
-  getKeys: function(obj) {
-    return Object.keys(obj);
-  }
-};
+function getKeys(obj) {
+  return Object.keys(obj);
+}
 
 // Example Test Cases
-console.log(myObj.getKeys(student)); // Output: ["name"]
+console.log(getKeys(student)); // Output: ["name"]
 
-// Additional Test Cases
 const studentWithMoreProps = {
   name: "John",
   age: 20,
   city: "New York"
 };
-console.log(myObj.getKeys(studentWithMoreProps)); // Output: ["name", "age", "city"]
+console.log(getKeys(studentWithMoreProps)); // Output: ["name", "age", "city"]
 
 const emptyObject = {};
-console.log(myObj.getKeys(emptyObject)); // Output: []
+console.log(getKeys(emptyObject)); // Output: []
 
 const singleProperty = { hobby: "Reading" };
-console.log(myObj.getKeys(singleProperty)); // Output: ["hobby"]
+console.log(getKeys(singleProperty)); // Output: ["hobby"]
