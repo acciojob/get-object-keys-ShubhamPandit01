7dc0817c2f7af88fa2ce259bfb314fe53a9e3624
Shubham Pandit <Shubhampandit75572@gmail.com>
2025-03-23 13:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-// Create the student object
-const student = {
+// Create the student object with a getKeys method
+const myObj = {
   name: "John",
   age: 20,
-  city: "New York"
-};
-
-// Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-// Attach the function to myObj for Cypress
-const myObj = {
-  ...student,
+  city: "New York",
   getKeys: function () {
-    return getKeys(this);
+    return Object.keys(this);
   }
 };
 
 // Example Test Cases
 console.log(myObj.getKeys()); // Output: ["name", "age", "city"]
 
+// Single property test
 const singleProperty = {
   hobby: "Reading",
   getKeys: function () {
-    return getKeys(this);
+    return Object.keys(this);
   }
 };
 console.log(singleProperty.getKeys()); // Output: ["hobby"]
 
+// Empty object test
 const emptyObject = {
   getKeys: function () {
-    return getKeys(this);
+    return Object.keys(this);
   }
 };
 console.log(emptyObject.getKeys()); // Output: []
