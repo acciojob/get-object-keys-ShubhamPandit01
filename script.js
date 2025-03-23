136843b1f4019491dff0cee4b3af47c6e1fa978b
Shubham Pandit <Shubhampandit75572@gmail.com>
2025-03-23 13:09:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 // Create the student object
 const student = {
   name: "John",
+  age: 20,
+  city: "New York"
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Attach the function to myObj for Cypress
+const myObj = {
+  ...student,
   getKeys: function () {
-    return Object.keys(this);
+    return getKeys(this);
   }
 };
 
 // Example Test Cases
-console.log(student.getKeys()); // Output: ["name"]
-
-const studentWithMoreProps = {
-  name: "John",
-  age: 20,
-  city: "New York",
-  getKeys: function () {
-    return Object.keys(this);
-  }
-};
-console.log(studentWithMoreProps.getKeys()); // Output: ["name", "age", "city"]
-
-const emptyObject = {
-  getKeys: function () {
-    return Object.keys(this);
-  }
-};
-console.log(emptyObject.getKeys()); // Output: []
+console.log(myObj.getKeys()); // Output: ["name", "age", "city"]
 
 const singleProperty = {
   hobby: "Reading",
   getKeys: function () {
-    return Object.keys(this);
+    return getKeys(this);
   }
 };
 console.log(singleProperty.getKeys()); // Output: ["hobby"]
+
+const emptyObject = {
+  getKeys: function () {
+    return getKeys(this);
+  }
+};
+console.log(emptyObject.getKeys()); // Output: []
